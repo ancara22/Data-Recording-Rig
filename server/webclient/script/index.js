bf8a01ebb3c90428a36370a1b7044d97d22b00a8
Dionis Barcari <dionisbarcari@Dioniss-MacBook-Pro.local>
2023-11-16 14:58:25 +0000

    Web frontend is done/no EEG interface

--- server/webclient/script/index.js
+++ server/webclient/script/index.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-        pageContent: 'data',
+        pageContent: 'settings',
         graphInterval: null,
         rigActive: false,
         statusText: 'ONLINE',
@@ -12,8 +12,7 @@ const app = new Vue({
             this.getGSRdata();
         } else {
             clearInterval(this.graphInterval);
-        }
-          
+        } 
     },
     watch: {
         pageContent: function(newPage, oldPage) {
@@ -27,10 +26,7 @@ const app = new Vue({
                 this.graphInterval = setInterval(()=> {
                     this.getGSRdata()
                 },1000)
-
             } 
-            
-            
         }
     },
 
@@ -54,7 +50,6 @@ const app = new Vue({
                 let layout = { title: 'GSR Data Graph', xaxis: { title: 'Timestamp' }, yaxis: { title: 'GSR' } };
                 let trace = { type: 'scatter', mode: 'lines', x: data.gsr_data.map(row => new Date(row[0])), y: data.gsr_data.map(row => row[1]) };
                 Plotly.newPlot('gsrGraph', [trace], layout);
-             
             })
             .catch(error => console.error('Error fetching GSR data:', error));
        }
